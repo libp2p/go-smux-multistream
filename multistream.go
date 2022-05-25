f26953966b2305ac4b882874d497add66fb5a3e1
Marten Seemann <martenseemann@gmail.com>
2022-05-25 20:22:14 +0200

    deprecate this repo

--- multistream.go
+++ multistream.go
@@ -1,75 +1,20 @@
 // Package multistream implements a peerstream transport using
 // go-multistream to select the underlying stream muxer
+//
+// Deprecated: This package has moved into go-libp2p as a sub-package: github.com/libp2p/go-libp2p/p2p/muxer/muxer-multistream.
 package multistream
 
 import (
-	"fmt"
-	"net"
-	"time"
-
-	"github.com/libp2p/go-libp2p-core/network"
-
-	mss "github.com/multiformats/go-multistream"
+	muxer_multistream "github.com/libp2p/go-libp2p/p2p/muxer/muxer-multistream"
 )
 
-var DefaultNegotiateTimeout = time.Second * 60
+// Deprecated: use github.com/libp2p/go-libp2p/p2p/muxer/muxer-multistream.DefaultNegoiateTimeout instead.
+var DefaultNegotiateTimeout = muxer_multistream.DefaultNegotiateTimeout
 
-type Transport struct {
-	mux *mss.MultistreamMuxer
+// Deprecated: use github.com/libp2p/go-libp2p/p2p/muxer/muxer-multistream.Transport instead.
+type Transport = muxer_multistream.Transport
 
-	tpts map[string]network.Multiplexer
-
-	NegotiateTimeout time.Duration
-
-	OrderPreference []string
-}
-
+// Deprecated: use github.com/libp2p/go-libp2p/p2p/muxer/muxer-multistream.NewBlankTransport instead.
 func NewBlankTransport() *Transport {
-	return &Transport{
-		mux:              mss.NewMultistreamMuxer(),
-		tpts:             make(map[string]network.Multiplexer),
-		NegotiateTimeout: DefaultNegotiateTimeout,
-	}
-}
-
-func (t *Transport) AddTransport(path string, tpt network.Multiplexer) {
-	t.mux.AddHandler(path, nil)
-	t.tpts[path] = tpt
-	t.OrderPreference = append(t.OrderPreference, path)
-}
-
-func (t *Transport) NewConn(nc net.Conn, isServer bool, scope network.PeerScope) (network.MuxedConn, error) {
-	if t.NegotiateTimeout != 0 {
-		if err := nc.SetDeadline(time.Now().Add(t.NegotiateTimeout)); err != nil {
-			return nil, err
-		}
-	}
-
-	var proto string
-	if isServer {
-		selected, _, err := t.mux.Negotiate(nc)
-		if err != nil {
-			return nil, err
-		}
-		proto = selected
-	} else {
-		selected, err := mss.SelectOneOf(t.OrderPreference, nc)
-		if err != nil {
-			return nil, err
-		}
-		proto = selected
-	}
-
-	if t.NegotiateTimeout != 0 {
-		if err := nc.SetDeadline(time.Time{}); err != nil {
-			return nil, err
-		}
-	}
-
-	tpt, ok := t.tpts[proto]
-	if !ok {
-		return nil, fmt.Errorf("selected protocol we don't have a transport for")
-	}
-
-	return tpt.NewConn(nc, isServer, scope)
+	return muxer_multistream.NewBlankTransport()
 }
